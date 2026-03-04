6b52919f1e65f30359c18664a7a4173b6160d727
Negro1-eng <ulises.garcia.ahedo@gmail.com>
2026-03-03 20:10:32 -0600

    Refactor web_app.py to update Google Sheets ID

--- web_app.py
+++ web_app.py
@@ -66,7 +66,7 @@ for key in ["beneficiario", "clc", "contrato", "factura"]:
     st.session_state.setdefault(key, "")
 
 # ================= GOOGLE SHEETS =================
-ID_SHEET = "1RKjYKBPcvbxul2WgRi72DpOBwB0XZwQcFyAY9o6ldOo"
+ID_SHEET = "1y66tbEhZ4E0XGPuUBB3O21A-56T_KAQs"
 
 @st.cache_data
 def cargar_datos():
@@ -287,6 +287,7 @@ st.download_button(
     convertir_excel(tabla),
     file_name="resultados_pagos.xlsx"
 )
+
 
 
 
